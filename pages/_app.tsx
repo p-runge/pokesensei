8e9e701f8d1dcee6df75a0ac0e412b063504fc61
Philipp <me@p6.gg>
2022-06-20 23:50:19 +0200

    feat: add material ui and basic homepage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const theme = createTheme({});
+
+function App({ Component, pageProps }: AppProps) {
+  const title = 'PokéSensei'
+  const metaDescription = 'Improve your Pokémon knowledge'
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={metaDescription} />
+        <link rel="icon" href="/favicon.ico" />
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+        <link
+          rel="preload"
+          as="font"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        </Head>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
-export default MyApp
+export default App
