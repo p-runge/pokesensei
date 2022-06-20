33ce6dd036ce174e6709ae1dbf8656c5f5eb49af
Philipp <me@p6.gg>
2022-06-21 00:25:25 +0200

    feat: set up linter and prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,22 @@
-import { Box, Button, Container, Typography } from '@mui/material'
-import type { NextPage } from 'next'
+import { Box, Button, Container, Typography } from "@mui/material";
+import type { NextPage } from "next";
 
-const Home: NextPage = (props) => {
-  const title = 'PokéSensei'
-  const copy = 'Here you can test your Pokémon knowledge and see if you\'re a true Pokémon master!'
+const Home: NextPage = () => {
+  const title = "PokéSensei";
+  const copy =
+    "Here you can test your Pokémon knowledge and see if you're a true Pokémon master!";
 
   return (
     <Container component="main">
       <Box>
         <Typography variant="h1">{title}</Typography>
         <Typography>{copy}</Typography>
-        <Button variant="contained" size="large">Start</Button>
+        <Button variant="contained" size="large">
+          Start
+        </Button>
       </Box>
     </Container>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
