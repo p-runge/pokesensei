451ac6e1774a979c2840d297319ae3ab9ca284e8
Philipp <me@p6.gg>
2022-07-07 19:46:59 +0200

    feat: update url resolutions for ssr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -36,8 +36,9 @@ export default withTRPC<AppRouter>({
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
+    const apiPath = "/api/trpc";
     const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
+      ? `https://${process.env.VERCEL_URL}/${apiPath}`
       : "http://localhost:3000/api/trpc";
 
     return {
@@ -51,5 +52,5 @@ export default withTRPC<AppRouter>({
   /**
    * @link https://trpc.io/docs/ssr
    */
-  ssr: false,
+  ssr: true,
 })(App);
