fe1e1fca7b7258097ca8c4acc0d7b12908d28108
Philipp <me@p6.gg>
2023-12-06 00:32:14 +0100

    prepare evaluation page

---
+++ src/app/[locale]/play/evaluate/_components/evaluated-question.tsx
@@ -0,0 +1,57 @@
+import { useTranslations } from "next-intl";
+import Image from "next/image";
+import type { QuestionWithAnswers } from "~/server/utils/question";
+
+export interface EvaluatedQuestionData {
+  question: QuestionWithAnswers;
+  givenAnswer: string;
+  correctAnswers: string[];
+}
+
+export default function EvaluatedQuestion({
+  data: { question, givenAnswer, correctAnswers },
+}: {
+  data: EvaluatedQuestionData;
+}) {
+  const t = useTranslations();
+
+  return (
+    <div className="m-auto mb-12 grid max-w-full gap-4">
+      {/* question */}
+      <div className="flex flex-col items-center justify-center rounded-lg bg-gray-700 p-4">
+        <span>
+          {t(question.question.label.string, question.question.label.params)}
+        </span>
+        {question.question.type === "NAME_OF_POKEMON_BY_IMAGE" && (
+          <Image
+            src={question.question.imgSrc}
+            alt=""
+            width="192"
+            height="192"
+            className="rendering-pixelated"
+            priority
+          />
+        )}
+      </div>
+
+      {/* answers */}
+      <div className="grid grid-cols-2 gap-4">
+        {question.answers.map((answer) => (
+          <div
+            className={`btn cursor-default rounded-lg border-4 border-transparent p-3 ${
+              (correctAnswers.includes(answer.value) && "bg-success") ||
+              (!correctAnswers.includes(answer.value) &&
+                answer.value === givenAnswer &&
+                "bg-error") ||
+              "bg-primary"
+            } ${
+              answer.value === givenAnswer ? "border-4 border-secondary" : ""
+            }`}
+          >
+            <span>{answer.label}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/app/[locale]/play/evaluate/page.tsx
+++ src/app/[locale]/play/evaluate/page.tsx
@@ -1,7 +1,61 @@
+import { useTranslations } from "next-intl";
+import EvaluatedQuestion, {
+  type EvaluatedQuestionData,
+} from "./_components/evaluated-question";
+import React from "react";
+import Link from "~/components/link";
+
 export default function Evaluate() {
+  const t = useTranslations();
+
+  // TODO: fetch data from api
+  const data = [] as any[];
+
   return (
-    <div>
-      <h1>Evaluate</h1>
+    <div className="w-full">
+      <p>
+        {t("quiz_evaluation_absolute", {
+          // TODO: calculate based no correct answers
+          correct: true,
+          max: data.length,
+        })}
+      </p>
+      <p>
+        {t("quiz_evaluation_percentage", {
+          percentage: Math.round(
+            (100 / data.length || 1) *
+              // data.filter((item) =>
+              //   item.correctAnswers.includes(item.givenAnswer),
+              // ).length,
+              data.length || 1,
+          ),
+        })}
+      </p>
+
+      <div className="pb-6" />
+
+      {data.map((evaluatedQuestion, i) => (
+        <React.Fragment key={`evaluation-question-${i}`}>
+          <p>{t("evaluation_question_title", { id: i + 1 })}</p>
+          <div className="pb-2" />
+          <EvaluatedQuestion
+            // TODO: remove cast
+            data={evaluatedQuestion as EvaluatedQuestionData}
+          />
+        </React.Fragment>
+      ))}
+
+      <div className="pb-6" />
+
+      <div className="flex gap-2">
+        <Link href="/setup" variant="primary">
+          {t("quiz_change_setup_button")}
+        </Link>
+
+        <Link href="/play" variant="primary">
+          {t("quiz_replay_button")}
+        </Link>
+      </div>
     </div>
   );
 }
