a6d167aa12453550bad4deb90fe74ed60c5741dd
Philipp <me@p6.gg>
2023-12-08 20:44:01 +0100

    add loading state to play page

---
+++ src/app/[locale]/play/loading.tsx
@@ -0,0 +1,9 @@
+import Loader from "~/components/Loader";
+
+export default function Loading() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Loader />
+    </div>
+  );
+}
