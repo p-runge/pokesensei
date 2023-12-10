b341c782d0b7c8c864724e1dbc04370bd27267b4
Philipp <me@p6.gg>
2023-12-10 16:49:33 +0100

    fix locale selector href

--- src/components/locale-select.tsx
+++ src/components/locale-select.tsx
@@ -1,17 +1,21 @@
+"use client";
+
 import { useLocale } from "next-intl";
 import Image from "next/image";
 import { LOCALES } from "~/i18n";
 import { cn } from "~/server/utils/cn";
 import Link from "./link";
+import { usePathname } from "~/navigation";
 
 const LocaleSelect = () => {
   const activeLocale = useLocale();
+  const pathname = usePathname();
 
   return (
     <div className="grid grid-flow-col grid-rows-1 gap-2">
       {LOCALES.map((locale) => (
         <Link
-          href="/"
+          href={pathname}
           locale={locale}
           className={cn(
             "border-2",
