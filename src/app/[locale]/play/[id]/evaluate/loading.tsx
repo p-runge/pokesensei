06ab6727954cc0859fcb9b869e92c3756f1a1974
Philipp <me@p6.gg>
2023-12-11 12:48:46 +0100

    make evaluate page a server component

---
+++ src/app/[locale]/play/[id]/evaluate/loading.tsx
@@ -0,0 +1,9 @@
+import Loader from "~/components/loader";
+
+export default function Loading() {
+  return (
+    <div className="flex grow items-center justify-center">
+      <Loader />
+    </div>
+  );
+}
