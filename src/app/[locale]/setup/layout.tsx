8e94438e9ad4c333a903e83c6d93fb28b053aec1
Philipp <me@p6.gg>
2024-03-26 21:58:48 +0100

    refactor: reinit project using create-t3-turbo

--- src/app/[locale]/setup/layout.tsx
+++
@@ -1,5 +0,0 @@
-import IntlProvider from "~/components/intl-provider";
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <IntlProvider>{children}</IntlProvider>;
-}
--- src/navigation.ts
+++
@@ -1,5 +0,0 @@
-import { createSharedPathnamesNavigation } from "next-intl/navigation";
-import { LOCALES } from "./i18n";
-
-export const { Link, redirect, usePathname, useRouter } =
-  createSharedPathnamesNavigation({ locales: LOCALES });
