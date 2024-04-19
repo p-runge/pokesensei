19e2f189a5417e5709c4a83cc71f9b51c0087737
Philipp <me@p6.gg>
2024-04-19 17:38:44 +0200

    clean up profile page

--- apps/nextjs/src/app/[locale]/profile/page.tsx
+++ apps/nextjs/src/app/[locale]/profile/page.tsx
@@ -11,8 +11,12 @@ export default function ProfilePage() {
     <NeedsAuth>
       <MainLayout>
         <h2 className="mb-6 text-center text-6xl">{t("page_profile_title")}</h2>
-        <div className="grid grid-cols-2">
-          <QuizHistoryList />
+        <div className="grid grid-cols-4">
+          {/* placeholder col */}
+          <div />
+          <div className="col-span-2">
+            <QuizHistoryList />
+          </div>
         </div>
       </MainLayout>
     </NeedsAuth>
